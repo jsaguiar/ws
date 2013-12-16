47f76d3658fdc4a54befecf9b9f6a9f0fa5a0067
pedrocarmona <pcarmona1990@gmail.com>
2013-12-16 00:43:15 +0000

    owl looks good

--- src/main/java/com/ws/data/Parser.java
+++ src/main/java/com/ws/data/Parser.java
@@ -49,17 +49,31 @@ public class Parser {
     }
 
     public static String refactor_spot_category(String aux){
-        aux=aux.replace(" / ","//");
-        aux=aux.replace(" & ","//");
-        aux=aux.replace("&","//");
+        aux=aux.replace(" / ",".");
+        aux=aux.replace("/",".");
+
+        aux=aux.replace(" & ","..");
+        aux=aux.replace("&","..");
+
+        aux=aux.replace(" ( ","LL.");
+        aux=aux.replace("(","LL.");
+
+        aux=aux.replace(" ) ",".LR");
+        aux=aux.replace(")",".LR");
+
+        aux=aux.replace(" ’ ",".....");
+        aux=aux.replace("'",".....");
+
         aux=aux.replace(" ","_");
+
+
         return aux;
     }
     private void addPointOfInterest(PointOfInterest poi){
         String category =  (poi.categories.get(0).getName());
         String category_id = poi.categories.get(0).getId();
         Resource spot = ontModel.createResource(namespace+poi._id)
-                .addProperty(RDF.type, ontModel.getProperty(namespace+category));
+                .addProperty(RDF.type, ontModel.getProperty(namespace+refactor_spot_category(category)));
 
 
         if(poi.name!=null){
