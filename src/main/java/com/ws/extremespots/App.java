784a9288100fd6ae10029b72e13dc18ffd3447c9
pedrocarmona <pcarmona1990@gmail.com>
2014-01-12 01:30:50 +0000

    semantic search start

--- src/main/java/com/ws/extremespots/App.java
+++ src/main/java/com/ws/extremespots/App.java
@@ -1,15 +1,19 @@
 package com.ws.extremespots;
 
 
+import com.ws.semantic.Search;
+
+/*
 import org.apache.jena.fuseki.Fuseki;
 import org.apache.jena.fuseki.server.SPARQLServer;
 import org.apache.jena.fuseki.server.ServerConfig;
+*/
 
 public class App extends Object {
 
 
     public static void main(String args[]) {
         // create an empty graph
-
+        Search s = new Search("best hotel in sidney");
     }
 }
