54156f83dce5fd41255d2d794fb94d17ea5f2d6e
pedrocarmona <pcarmona1990@gmail.com>
2014-01-19 08:18:43 +0000

    play framework

---
+++ ExtremeSpots/src/main/java/com/ws/extremespots/StaticVariables.java
@@ -0,0 +1,18 @@
+package com.ws.extremespots;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: pedrocarmona
+ * Date: 01/12/13
+ * Time: 22:45
+ * To change this template use File | Settings | File Templates.
+ */
+public class StaticVariables {
+    //public static String points_of_interest_json_file = "data/point_of_interests.json";
+    public static String points_of_interest_json_file = "data/point_of_interests274.json";
+    public static String owl_file = "owl/ws_ont.owl";
+    public static String triple_store_directory = "TripleStoreDirectory";
+    public static String ontology_name_space = "http://www.semanticweb.org/joao/ontologies/2013/10/POIs#";
+
+
+}
---
+++ ExtremeSpotsServer/build.sbt
@@ -0,0 +1,11 @@
+name := "ExtremeSpotsServer"
+
+version := "1.0-SNAPSHOT"
+
+libraryDependencies ++= Seq(
+  javaJdbc,
+  javaEbean,
+  cache
+)     
+
+play.Project.playJavaSettings
